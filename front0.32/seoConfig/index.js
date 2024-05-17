445405728508b12ca233326a5d30b33247d81c05
Alexis <72978378+Alexis-CAPON@users.noreply.github.com>
2024-05-17 21:10:17 +0200

    Update index.js

--- front0.32/seoConfig/index.js
+++ front0.32/seoConfig/index.js
@@ -1,12 +1,12 @@
 export default {
-  title: "ChatCBC - All in One Secured Generation AI",
+  title: "AOS - All in One Secured Generation AI",
   description:
     "A secured solution where you can retrieve data overall your systems and challenge it with AI.",
   og: {
-    title: "ChatCBC - All in One Secured Generation AI",
+    title: "AOS - All in One Secured Generation AI",
     description:
       "A secured solution where you can retrieve data overall your systems and challenge it with AI.",
     image: "https://logspot.vercel.app/logspot-banner.png",
-    url: "https://chatcbc.alexiscapon.com",
+    url: "https://aos.alexiscapon.com",
   },
 };
